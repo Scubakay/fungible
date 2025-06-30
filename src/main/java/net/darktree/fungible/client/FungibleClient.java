a802c585d9585b10fbb8ba7b0a546b7b2e32ce5a
Scubakay <=>
2025-06-30 16:49:12 +0200

    Fix a bunch of split sources issues?

---
+++ src/main/java/net/darktree/fungible/client/FungibleClient.java
@@ -0,0 +1,10 @@
+package net.darktree.fungible.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FungibleClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
